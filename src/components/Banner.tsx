b483049e3ac14acfb38f971fb22b7d411f661a4a
tuandtph13846 <tuandtph13846@fpt.edu.vn>
2022-04-08 17:08:33 +0700

    update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,33 +12,32 @@ const slideImages = [
 const Banner = (props: Props) => {
   return (
     <div className="banner">
-      <div>
+                      <div>
         <Slide easing="ease">
           <div className="each-slide">
-            <div style={{ 'backgroundImage': `url(${slideImages[0]})` }}>
-
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+              
             </div>
           </div>
           <div className="each-slide">
-            <div style={{ 'backgroundImage': `url(${slideImages[1]})` }}>
-
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            
             </div>
           </div>
           <div className="each-slide">
-            <div style={{ 'backgroundImage': `url(${slideImages[2]})` }}>
-
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              
             </div>
           </div>
         </Slide>
-
       </div>
-      <div className="banner-info text-center">
-        <i className="shipping" />
-        <h3>Shopping Place For Girls</h3>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enimcidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim</p>
-        <a href="/product">All Products</a>
-      </div>
-    </div>
+                <div className="banner-info text-center">
+                    <i className="shipping" />
+                    <h3>Shopping Place For Girls</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enimcidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim</p>
+                    <a href="/product">All Products</a>
+                </div>
+            </div>
   )
 }
 
